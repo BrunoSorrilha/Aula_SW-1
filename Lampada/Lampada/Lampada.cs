fd5d5b00ca3141b49fe618ddf219d7e217b094b4
Bruno_Sorrilha <124791792+BrunoSorrilha@users.noreply.github.com>
2024-02-29 12:42:35 -0300

    Add files via upload

--- Lampada/Lampada/Lampada.cs
+++ Lampada/Lampada/Lampada.cs
@@ -8,9 +8,45 @@ namespace Lampada
 {
     public class Lampada
     {
-        public bool Ligada { get; set; } 
-        public string? Cor { get; set; }
-        public int Potencia { get; set; }
+        private bool Ligada { get; set; } 
+        private string? Cor { get; set; }
+        private int Potencia { get; set; }
+
+        //Ligar/Desligar
+        public void ligar()
+        {
+            Ligada = true;
+        }
+        public void desligar()
+        {
+            Ligada = false;
+        }
+        public bool estaLigada()
+        {
+            return Ligada;
+        }
+
+        //Definir cor
+
+        public void Colores()
+        {
+            Cor = "Vermelho";
+        }
+        public string qualCor()
+        {
+            return Cor;
+        }
+
+        //Potencia da lampada
+
+        public void aopotencia()
+        {
+            Potencia = 200;
+        }
+        public int QualPotencia()
+        {
+            return Potencia;
+        }
 
 
     }
--- Lampada/Lampada/Program.cs
+++ Lampada/Lampada/Program.cs
@@ -6,29 +6,17 @@
         {
             //⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐INSTANCIA DE UM OBJETO⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐
             Lampada lampada1 = new Lampada();
-            Lampada lampada2 = new Lampada();
 
-            //⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐ACESSANDO UM ATRIBUTO PUBLICO DO OBJETO⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐
-            lampada1.Ligada = true;
-            lampada1.Potencia = 150;
-            lampada1.Cor = "Vermelha";
+            //chamando o metodo para ligar lamp1
+            lampada1.ligar();
+            lampada1.Colores();
+            lampada1.aopotencia();
 
-            lampada2.Ligada = false;
-            lampada2.Potencia = 10000;
-            lampada2.Cor = "amarelow";
-
-            //⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐VISUALIZANDO OS VALORES DE ATRIBUTOS⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐
-            Console.WriteLine("ESSA É A LAMPADA 1!!");
-            Console.WriteLine("A Lampada está ligada?" + lampada1.Ligada);
-            Console.WriteLine("Qual é a cor da Lampada?" + lampada1.Cor);
-            Console.WriteLine("Qual é a potencia da Lampada?" + lampada1.Potencia);
-            Console.WriteLine("* * * * * * * * * * * * * * * * * * * * * * * * * * * * ");
-            Console.WriteLine("ESSA É A LAMPADA 2!!");
-            Console.WriteLine("A Lampada está ligada?" + lampada2.Ligada);
-            Console.WriteLine("Qual é a cor da Lampada?" + lampada2.Cor);
-            Console.WriteLine("Qual é a potencia da Lampada?" + lampada2.Potencia);
-
-
+            //Chamando o metodo para verificar se lamp1 está ligada
+            Console.WriteLine("A lampada está ligada?" + lampada1.estaLigada());
+            Console.WriteLine("A cor da lampada é:" + lampada1.qualCor());
+            Console.WriteLine("A potencia é" + lampada1.QualPotencia());
+ 
         }
     }
 }
