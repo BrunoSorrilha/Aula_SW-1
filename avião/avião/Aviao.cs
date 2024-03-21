8644534b9125ad8d488b4daa7544820f58fe794b
Bruno_Sorrilha <124791792+BrunoSorrilha@users.noreply.github.com>
2024-03-21 11:16:55 -0300

    Add files via upload

---
+++ avião/avião/Aviao.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace avião
+{
+     class Aviao
+    {
+        private float Velocidade { get; set; }
+        private float Altura { get; set; }
+
+        public float Altura_Atual()
+        {
+            return Altura;
+        }
+        public float Velocidade_Atual()
+        {
+            return Velocidade;
+        }
+        public float Vrumm(float mudar_altura)
+        {
+            Altura += mudar_altura;
+            return Altura_Atual();
+        }
+        public float Ziumm(float marcos)
+        {
+            Velocidade += marcos;
+            return Velocidade_Atual();
+        }
+    }
+}
---
+++ avião/avião/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace avião
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("AVIÃO DA ETEC MCM");
+            Aviao aviao = new Aviao();
+
+            Console.WriteLine("A altura do avião é de: " + aviao.Altura_Atual() + " Metros de altura");
+            Console.WriteLine("A velocidade do avião é de: " + aviao.Velocidade_Atual() + "Km/h");
+
+            aviao.Vrumm(500);
+            aviao.Ziumm(500);
+            Console.WriteLine("A altura do avião é de: " + aviao.Altura_Atual() + " Metros de altura");
+            Console.WriteLine("A velocidade do avião é de: " + aviao.Velocidade_Atual() + "Km/h");
+
+        }
+    }
+}
